0a06f0b7fff0fae76abb8884323a6b465557dd11
Yanko Aleksandrov <yanko@idrobots.com>
2023-04-23 13:40:33 +0300

    updated loop

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -1,24 +1,3 @@
-// TODO Use browserify to load the compiled modules in the client. browserify main.js -o bundle.js
-/*In the preceding command, main.js is the file that contains the root module within our application's dependency tree.
- The bundle.js file is the output file that we will be able to import using a HTML script tag.
- Import the bundle.js file using a HTML <script> tag.*/
-
-function getElementByAttribute(attr, value, root) {
-    root = root || document.body;
-    if(root.hasAttribute(attr) && root.getAttribute(attr) == value) {
-        return root;
-    }
-    let children = root.children,
-        element;
-    for(let i = children.length; i--; ) {
-        element = getElementByAttribute(attr, value, children[i]);
-        if(element) {
-            return element;
-        }
-    }
-    return null;
-}
-
 let port_number = 3000;
 
 let socket = io('http://localhost:' + port_number, {
@@ -29,22 +8,13 @@ let socket = io('http://localhost:' + port_number, {
 
 let fbBtnClicked = false;
 
-console.log('script started click addmefast like in 8 sec');
+console.log('script started click addmefast like in 15 sec');
+
 
 setInterval(() => {
     if (fbBtnClicked && window.location.host === 'addmefast.com') {
         console.log('confirm like in 5 sec');
         setTimeout(() => {
-
-            console.log('check successLike in 5 sec');
-            setTimeout(() => {
-                let successLike = document.getElementsByClassName('success_like')[0]?.click();
-                if (successLike) {
-                    console.log('emit successLike');
-                    io.emit('successLike');
-                }
-            }, 5000);
-            
             console.log('addmefast click CONFIRM like');
             document.getElementsByClassName('single_like_button')[0]?.click();
         }, 5000);
@@ -53,7 +23,7 @@ setInterval(() => {
         console.log('addmefast click Like');
         document.getElementsByClassName('single_like_button')[0]?.click();
     }
-}, 8000);
+}, 15000);
 
 setInterval(() => {
     if (!fbBtnClicked && window.location.host === 'www.facebook.com') {
@@ -74,8 +44,6 @@ setInterval(() => {
         if (!likeBtn){
             likeBtn = document.querySelector('[aria-label="Follow"]');
         }
-        
-        
         else {
             console.log('facebook btn not found exit in 3 sec');
             setTimeout(() => {
--- index.js
+++ index.js
@@ -3,33 +3,77 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+let exec = require('child_process').exec;
 const io = new Server(server, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"],
+
         transports: ['websocket', 'polling'],
         credentials: true
     },
     allowEIO3: true
 });
-const {  spawn } = require('node:child_process');
+const { spawn } = require('node:child_process');
+const { CLIENT_RENEG_LIMIT } = require('tls');
 let successLike = 0;
+let runs = 1;
+let currentUser = process.argv[2] || 1;
+let loop = (process.argv[3] === "loop"); 
 
-spawn('google-chrome', [
-    '--disable-web-security',
-    '--allow-running-insecure-content',
-    '--nosession',
-    '--no-sandbox',
-    '--user-data-dir="/home/idr/Projects/social-robot-chrome-1"'
-]);
+let lastUser = 16;
 
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+    // console.log('listening on *:3000');
 });
 
-
-io.on('successLike', () => {
+io.on('successLikeRes', () => {
     successLike++;
     console.log('successLike count', successLike);
 });
 
+console.log('browser will restart in 50 sec');
+
+async function main() {
+    console.log('start browser...')
+    await sleep(2000);
+    startBrowser();
+    if(loop) {
+        console.log('MAIN RUNS', runs++);
+        console.log('currentUser: ', currentUser);
+        await sleep(50000); // 50 sec (50000)
+        exec('killall opera');
+        currentUser++;
+    }
+    
+   
+    
+    if (currentUser > lastUser && loop) {
+        currentUser = 1;
+        console.log('kill browser start again in 4 hours');
+        await sleep(14400000); // 4 hours
+    }
+
+    if(loop) {
+        main() 
+    }
+    
+
+ 
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function startBrowser() {
+    spawn('opera', [
+        '--disable-web-security',
+        '--allow-running-insecure-content',
+        '--nosession',
+        '--no-sandbox',
+        `--user-data-dir="/home/idr/Projects/addmefast-bot-1/browser/social-robot-chrome-${currentUser}"`
+    ]);
+}
+
+main()
